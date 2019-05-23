0dc93df464bc7b66f7554d6455897bee9b4aac4f
sharky <zeebing@gmail.com>
2019-05-22 21:19:30 -0400

    made a Note Schema

---
+++ models/Note.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+
+//built in Schema constructor
+var Schema = mongoose.Schema;
+
+//make new schema for a NoteSchema object
+var NoteSchema = new Schema({
+    title: String,
+    body: String
+});
+
+//make model using schema
+var Note = mongoose.model("Note", NoteSchema);
+
+//export model
+module.exports = Note;
