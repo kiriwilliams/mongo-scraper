b33ed0179fb5e467ce8d64764dcd805ed1f99478
sharky <zeebing@gmail.com>
2019-05-22 23:58:56 -0400

    fixed grabbing val from textarea

--- public/app.js
+++ public/app.js
@@ -28,15 +28,14 @@ $(document).on("click", ".modalToggler", function() {
 $(document).on("click", ".saveNote", function() {
   // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
+  var noteId = thisId+"noteInput";
+  console.log($("#"+noteId).val().trim());
   // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
     method: "POST",
     url: "/articles/" + thisId,
     data: {
-      // Value taken from title input
-      title: $("#titleinput").val(),
-      // Value taken from note textarea
-      body: $("#bodyinput").val()
+      note: $("#"+noteId).val().trim()
     }
   })
     // With that done
